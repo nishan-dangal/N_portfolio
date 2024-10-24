5259b56d69c46a206585f19ceaf6b9ea6646bdf9
Nis3D <103100755+Nis3D@users.noreply.github.com>
2024-10-24 13:53:10 +0800

    Changes made in the button like styling such as hover colour, button colour and bg color

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -16,7 +16,7 @@ const HeroSection = () => {
           className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
         >
           <div className="text-white mb-4 text-1xl sm:text-1xl lg:text-4xl lg:leading-normal font-extrabold">
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-blue-500">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-pink-600">
               Hello, I&apos;m{" "}
             </span>
             <br></br>
@@ -39,14 +39,14 @@ const HeroSection = () => {
           <div>
             <Link
               href="/#contact"
-              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-blue-500 hover:bg-slate-200 text-white"
+              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-purple-500 to-blue-500 hover:bg-slate-800 text-white"
             >
               Hire Me
             </Link>
             <a href="/resume.pdf" target="_blank" rel="nopener noreferrer"
-            className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
+            className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-purple-500 to-blue-500 hover:bg-blue-200 text-white mt-3"
             >
-              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+              <span className="block bg-[#121212] hover:bg-white-400 rounded-full px-5 py-2">
                 Download Resume
                </span>
             </a>
