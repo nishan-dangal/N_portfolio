58e597d0c87f3d6b8d847b74e1ffac6a172ddefe
Nis3D <103100755+Nis3D@users.noreply.github.com>
2024-10-30 12:27:50 +0800

    Removing menuoverlay jsx component to resolve the error

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import React, { useState } from "react";
 import NavLink from "./NavLink";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
-import MenuOverlay from "./MenuOverlay";
+// import MenuOverlay from "./MenuOverlay";
 
 const navLinks = [
   {
