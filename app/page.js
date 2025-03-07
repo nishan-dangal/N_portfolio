33ca7eb56b9014a1f3058491e97e891fca869532
Nis3D <103100755+Nis3D@users.noreply.github.com>
2025-03-07 15:55:31 +0800

    correction of react component naming convention

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 import HeroSection from "./components/HeroSection";
-import Navbar from "./components/NavBar";
+import NavBar from "./components/NavBar";
 import AboutSection from "./components/AboutSection";
 import ProjectsSection from "./components/ProjectsSection";
 import EmailSection from "./components/EmailSection";
@@ -9,7 +9,7 @@ import SkillsSection from "./components/SkillsSection";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#14282e] " >
-      <Navbar />
+      <NavBar />
       <div className="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
         <SkillsSection/>
